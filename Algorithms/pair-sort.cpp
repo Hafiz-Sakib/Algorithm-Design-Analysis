327ef3fc165bc673135ad6c817be6e03ff003137
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-12-20 08:57:26 +0600

    update

--- Algorithms/pair-sort.cpp
+++ Algorithms/pair-sort.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int main()
 {
     vector<pair<int, int>> v = {{1, 2}, {5, 4}, {3, 2}, {1, 3}, {5, 6}, {3, 8}};
+    cout << "Before Sorting : " << endl;
     for (auto u : v)
     {
         cout << u.first << ' ' << u.second << endl;
     }
     sort(v.begin(), v.end());
+    cout << "After Sorting : " << endl;
     for (auto u : v)
     {
         cout << u.first << ' ' << u.second << endl;
